a82efcb22f431d1f4b667248d0b2b5e8f7d3d62d
Wim Thys <wim.thys@zardof.be>
2011-12-28 00:34:51 +0100

    Switched to 80 char width and added some warnings for invalid usages

--- src/be/zardof/divecraft/DiveCraft.java
+++ src/be/zardof/divecraft/DiveCraft.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
@@ -14,20 +15,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author Wim Thys
- *
+ * 
  */
 public class DiveCraft extends JavaPlugin {
-	
+
 	protected FileConfiguration _config;
-	
+
 	Logger log = Logger.getLogger("Minecraft");
-	
+
 	DiveCraftDamageListener _dcdl = new DiveCraftDamageListener(this);
 	DiveCraftDeathListener _dcdeath = new DiveCraftDeathListener(this);
-	
+
 	Map<Integer, Integer> _helmets = new HashMap<Integer, Integer>();
 	private int _diveFuel;
-	
 
 	@Override
 	public void onDisable() {
@@ -37,23 +37,32 @@ public class DiveCraft extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		_config = getConfig();
-		
-		for (Map<String, Object> helmet: _config.getMapList("helmets")) {
+
+		for (Map<String, Object> helmet : _config.getMapList("helmets")) {
 			Integer helmet_id = (Integer) helmet.get("item");
 			Integer helmet_usage = (Integer) helmet.get("usage");
-			_helmets.put(helmet_id, helmet_usage);
+
+			if (helmet_usage.intValue() < 1) {
+				log.warning("Helmet `" + helmet_id + "' has a usage smaller "
+						+ "than 1, it will not be added as a diving helmet");
+				continue;
+			} else {
+				_helmets.put(helmet_id, helmet_usage);
+			}
 		}
-		
+
 		_diveFuel = _config.getInt("fuel");
-		
+
 		_config.options().copyDefaults(true);
 		saveConfig();
-		
+
 		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type.ENTITY_DAMAGE, _dcdl, Event.Priority.Normal, this);
-		pm.registerEvent(Event.Type.ENTITY_DEATH, _dcdeath, Event.Priority.Normal, this);
-		
-		log.info("Enabled " + getVersionString() );
+		pm.registerEvent(Event.Type.ENTITY_DAMAGE, _dcdl,
+				Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.ENTITY_DEATH, _dcdeath,
+				Event.Priority.Normal, this);
+
+		log.info("Enabled " + getVersionString());
 	}
 
 	private String getVersionString() {
@@ -63,15 +72,15 @@ public class DiveCraft extends JavaPlugin {
 	public boolean isDiveHelmet(int typeId) {
 		return _helmets.containsKey(typeId);
 	}
-	
+
 	public int getHelmetUsage(int typeId) throws InvalidHelmetException {
 		if (isDiveHelmet(typeId)) {
 			return _helmets.get(typeId).intValue();
 		}
-		
+
 		throw new InvalidHelmetException();
 	}
-	
+
 	public int getDiveFuel() {
 		return _diveFuel;
 	}
